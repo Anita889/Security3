a41a264b27562112a1216a0caa142944f0c158a4
Anita889 <manahit889@gmail.com>
2023-12-01 21:03:35 +0400

    Myyy

---
+++ src/main/java/org/example/HuffmanNode.java
@@ -0,0 +1,18 @@
+package org.example;
+
+class HuffmanNode implements Comparable<HuffmanNode> {
+    char data;
+    int frequency;
+    HuffmanNode left, right;
+
+    public HuffmanNode(char data, int frequency) {
+        this.data = data;
+        this.frequency = frequency;
+        left = right = null;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode other) {
+        return this.frequency - other.frequency;
+    }
+}
